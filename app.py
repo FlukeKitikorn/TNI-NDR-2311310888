a87aadddd03c270ebcfd54cd31d7e45cea6618d2
Kitikorn Khanthasen <evillgodmrsupersonic@gmail.com>
2025-05-21 21:30:11 +0700

    feat(show_data_from_api) : add show data by selection

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import streamlit as st
+import json
 from src.components.header import show_header  
 from src.components.input import  search_box
 from src.components.content import candle_chart
+from Datahandle.settrade.data import get_candlestick
+from Datahandle.settrade.config import API_ID, API_KEY, ACC_NO, BORKER_ID, APP_CODE
 
+# def main():
+#     st.sidebar.title("เมนูหลัก")
+#     menu = st.sidebar.selectbox("เลือกเมนู", ["หน้า UI", "หน้าอื่น"])
 
-def main():
-    st.sidebar.title("เมนูหลัก")
-    menu = st.sidebar.selectbox("เลือกเมนู", ["หน้า UI", "หน้าอื่น"])
-
-    if menu == "หน้า UI":
-        show_header()
-    else:
-        st.write("นี่คือหน้าอื่น")
+#     if menu == "หน้า UI":
+#         show_header()
+#     else:
+#         st.write("นี่คือหน้าอื่น")
 
 def color_bg():
     page_bg_color = '''
@@ -23,12 +25,20 @@ def color_bg():
     '''
     return page_bg_color
 
+def show_data_from_api(symbol, period):
+    result = get_candlestick(symbol, interval=period, limit=5)
+    if result:
+        print(json.dumps(result, indent=4))
+    else:
+        print("No data returned.")
+
 if __name__ == "__main__":
 
     st.set_page_config(page_title='SWAST - Handover Delays',  layout='wide')
     st.markdown(color_bg(), unsafe_allow_html=True)
     show_header()
     st.divider()
-    search_box()
+    symbol, indicators, period = search_box()
     candle_chart()
+    show_data_from_api(symbol, period)
     # main()
--- src/components/input.py
+++ src/components/input.py
@@ -1,37 +1,53 @@
 import streamlit as st
 from src.controller.loadjson import load_stock_list
+import json
+import uuid
 
 def search_box():
     col1, col2, col3, col4 = st.columns([1, 1, 1, 2])
     stock_list = load_stock_list()
-    
-    with col1:    
+
+    with col1:   
         options = [f"{stock['symbol']} | {stock['name']}" for stock in stock_list]
-        selected_stock = st.selectbox("เลือกหุ้น:", options, index=None)
+        selected_stock = st.selectbox("เลือกหุ้น:", options, index=None, key="stock_select")
+        choice = None
+        
         if selected_stock:
-            choice = selected_stock
+            choice = selected_stock.split(" | ")[0] 
         # st.write("You selected:", types)
     st.header(selected_stock if selected_stock else "กรุณาเลือกหุ้น")
     with col2:
-        indicators = st.multiselect(
+        indicator = st.multiselect(
             "Indicator:",
-            ["SMA5", "SMA10", "EMA", "MACD"]
+            ["SMA5", "SMA10", "EMA", "MACD"],
+            key="indicator_select"
         )
         # st.write("You selected:", indicators)
 
     with col3:
         options_period = {
-            0: "1H",
-            1: "1D",
-            2: "1M",
-            3: "6M",
+            '60m': "1H",
+            '1d': "1D",
+            '1M': "1M",
+            '6M': "6M",
         }
         selection = st.segmented_control(
                     "Tool",
                     options=options_period.keys(),
                     format_func=lambda option: options_period[option],
                     selection_mode="single",
+                    key="period_select"
                 )
     
     with col4:
-        st.write("")  
+        st.write("") 
+    
+    '''
+    print(json.dumps({
+            "choice": choice,
+            "indicator": indicator,
+            "period": selection
+        }, ensure_ascii=False, indent=2))
+    '''
+
+    return choice ,indicator, selection if selection is not None else None
