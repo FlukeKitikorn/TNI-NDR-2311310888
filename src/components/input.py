62d28e4f0d892c1856535576bf60d99909494155
Kitikorn Khanthasen <evillgodmrsupersonic@gmail.com>
2025-05-21 16:09:22 +0700

    feat(stock_list) : list by choice button

--- src/components/input.py
+++ src/components/input.py
@@ -1,15 +1,16 @@
 import streamlit as st
+from src.controller.loadjson import load_stock_list
 
 def search_box():
     col1, col2, col3 = st.columns([1, 1, 3])
 
-    st.header("MALEE : บริษัท มาลีกรุ๊ป จำกัด (มหาชน)")
+    # st.header("MALEE : บริษัท มาลีกรุ๊ป จำกัด (มหาชน)")
     with col1:
-        types = st.selectbox(
-            "Contact Method:",
-            ("Email", "Home phone", "Mobile phone"),
-            index=None
-        )
+        stock_list = load_stock_list()
+        options = [f"{stock['symbol']} | {stock['name']}" for stock in stock_list]
+        selected_stock = st.selectbox("เลือกหุ้น:", options, index=None)
+        if selected_stock:
+            st.write(f"คุณเลือก: {selected_stock}")
         # st.write("You selected:", types)
 
     with col2:
