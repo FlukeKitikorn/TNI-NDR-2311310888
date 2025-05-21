ed8f45d68637787a4f25c6777a64a4af3c0ebaa1
Kitikorn Khanthasen <evillgodmrsupersonic@gmail.com>
2025-05-21 19:39:30 +0700

    style(search_box) : change col3 radio to segmented_control

--- src/components/input.py
+++ src/components/input.py
@@ -2,24 +2,36 @@ import streamlit as st
 from src.controller.loadjson import load_stock_list
 
 def search_box():
-    col1, col2, col3 = st.columns([1, 1, 3])
-
-    # st.header("MALEE : บริษัท มาลีกรุ๊ป จำกัด (มหาชน)")
-    with col1:
-        stock_list = load_stock_list()
+    col1, col2, col3, col4 = st.columns([1, 1, 1, 2])
+    stock_list = load_stock_list()
+    
+    with col1:    
         options = [f"{stock['symbol']} | {stock['name']}" for stock in stock_list]
         selected_stock = st.selectbox("เลือกหุ้น:", options, index=None)
         if selected_stock:
-            st.write(f"คุณเลือก: {selected_stock}")
+            choice = selected_stock
         # st.write("You selected:", types)
-
+    st.header(selected_stock if selected_stock else "กรุณาเลือกหุ้น")
     with col2:
-        indicators = st.selectbox(
+        indicators = st.multiselect(
             "Indicator:",
-            ("SMA", "EMA", "MACD"),
-            index=None
+            ["SMA5", "SMA10", "EMA", "MACD"]
         )
         # st.write("You selected:", indicators)
 
     with col3:
+        options_period = {
+            0: "1H",
+            1: "1D",
+            2: "1M",
+            3: "6M",
+        }
+        selection = st.segmented_control(
+                    "Tool",
+                    options=options_period.keys(),
+                    format_func=lambda option: options_period[option],
+                    selection_mode="single",
+                )
+    
+    with col4:
         st.write("")  
