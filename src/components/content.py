c50ce8e08ecd9b9372231ca6f9e98e873d673c89
Kitikorn Khanthasen <evillgodmrsupersonic@gmail.com>
2025-05-23 00:07:22 +0700

    feat(candle_chart) : Add indicators parameter

--- src/components/content.py
+++ src/components/content.py
@@ -5,14 +5,14 @@ import json
 from src.components.chart import chart
 from Datahandle.settrade.data import get_candlestick
 
-def candle_chart(symbol, period, limit):
+def candle_chart(symbol, period, limit, indicators):
     col1, col2 = st.columns([2.5,1])
     data = np.random.randn(10, 1)
     result = get_candlestick(symbol, interval=period, limit=limit)
     # print(json.dumps(result, indent=4))
 
     with col1:
-        chart(result)
+        chart(result, indicators)
 
     with col2:
         st.write(data)  
