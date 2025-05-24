3c67c430bcff4114b33bd1175c4d831be3211214
Kitikorn Khanthasen <evillgodmrsupersonic@gmail.com>
2025-05-25 06:08:09 +0700

    fix(table) : change position

--- pages/import_excel.py
+++ pages/import_excel.py
@@ -3,7 +3,8 @@ import pandas as pd
 from Home import color_bg
 from src.controller.loadexcel import excel_to_pandas
 from src.controller.plot_graph import plot_price_trend
-
+from src.controller.loadexcel import table
+ 
 st.markdown(
     "<h1 style='text-align: center; color: white;'>Import stock</h1>",
     unsafe_allow_html=True
@@ -15,15 +16,10 @@ col1, col2, col3 = st.columns([0.5, 3, 0.5])
 
 with col2:
     uploaded_file = st.file_uploader("", type=["xlsx", "xls"])
+    data_clean = excel_to_pandas(uploaded_file)
+    # data_table = table(uploaded_file)
     if uploaded_file is not None:
-        try:
-            data_clean = excel_to_pandas(uploaded_file)
-            # แสดงผลเบื้องต้น
-            if data_clean.empty:
-                st.error("Data is empty after cleaning. Please check your input file.")
-            else:
-                fig = plot_price_trend(data_clean, title="KBANK Closing Price Trend")
-                st.pyplot(fig)
-        except Exception as e:
-            st.error(f"❌ Error loading Excel file: {e}")
-
+        fig = plot_price_trend(data_clean)
+        st.pyplot(fig)
+        table(uploaded_file)
+        
