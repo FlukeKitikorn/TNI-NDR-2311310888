1bd3e6c24cb71fb56b20d582206d405d45c2e3be
Kitikorn Khanthasen <evillgodmrsupersonic@gmail.com>
2025-05-25 07:04:46 +0700

    refactor(candle_chart)

--- src/components/content.py
+++ src/components/content.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
 import json
 from src.components.chart import chart
 from Datahandle.settrade.data import get_candlestick
@@ -8,11 +6,11 @@ from Datahandle.settrade.stat import summarize_stock_data
 
 def candle_chart(symbol, period, limit, indicators):
     col1, col2 = st.columns([3,1])
+
     result = get_candlestick(symbol, interval=period, limit=limit)
     # print(json.dumps(result, indent=4))
 
     if result is None:
-        # st.error("Failed to fetch candlestick data.")
         return
 
     stat = summarize_stock_data(result)
@@ -20,14 +18,15 @@ def candle_chart(symbol, period, limit, indicators):
     # print(json.dumps(stat))
     highest, lowest, average, total_vol, change, percent_change, last = stat
 
+
     with col1:
         chart(result, indicators)
-        
+
     with col2:
         if change is not None and percent_change is not None:
-            st.metric(label="Change", value=f"{change:+.2f} ({percent_change:+.2f}%)", delta=percent_change)
+            st.metric(label="Change", value=f"{change:+.2f} ({percent_change:+.2f}%)", delta=percent_change , border=True)
         else:
-            st.metric(label="Change", value="No data")
+            st.metric(label="Change", value="No data" , border=True)
         st.metric("Highest High", f"{highest:.2f} ฿", border=True)
         st.metric("Lowest Low", f"{lowest:.2f} ฿", border=True)
         st.metric("Total Volume", f"{total_vol:,.2f} ฿", border=True)
